3f1ad0302f2d5a49c5564b3ef06ebfe4e0c4d3b8
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-08-01 17:56:58 +0800

    [!] All the last commit have been removed due to some technical difficulties, finished the basic design.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import { withStyles } from '@material-ui/styles';
+import logo from './images/logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  back: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#b8d6ff',
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    paddingTop: '40px',
+  },
+  loginBox: {
+    width: '350px',
+    height: '350px',
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: "",
+      password: "",
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      tabs: e,
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.back}>
+        <Box>
+          <img src={logo} />
+        </Box>
+        <Paper className={classes.loginBox}>
+          test
+        </Paper>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
